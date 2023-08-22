b6989ec51a0c8ce61344e7f95382dc2d631eccdb
phant0mthief1417 <info4aakash@gmail.com>
2023-08-22 17:10:39 -0600

    first page done, some additional formatting and setup done, all thats left is to add the new pages and some css

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import logo from "./logo.png";
+
+import github from "./tech/github.png";
+import linkedin from "./tech/linkedin.png";
+
+export { logo, github, linkedin };
---
+++ src/constants/index.js
@@ -0,0 +1,31 @@
+import { github, linkedin } from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const heroLinks = [
+  {
+    src: github,
+    alt: "github",
+    link: "https://github.com/phant0mthief1417",
+  },
+  {
+    src: linkedin,
+    alt: "linkedin",
+    link: "https://www.linkedin.com/in/aakashdurairaj/",
+  },
+];
+
+export { heroLinks };
